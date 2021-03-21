cbdc1e74e2dd8d7cb768a47605e5c1f354d7551c
maryna1408 <cabanmarina9@gmail.com>
2021-03-21 23:05:30 +0200

    fixed mode-buttons + lamp

--- js/app.js
+++ js/app.js
@@ -10,12 +10,39 @@ optionsEl.addEventListener('click', event =>{
     if (optionsLampEl.dataset.action == 1) {
         interierLampEl.src = 'images/lamp1.webp'
         lampEl.src = 'images/lamp1.webp'
+        const siblings = findSiblings(optionsLampEl)
+        siblings.forEach(sibling => {
+            sibling.style.background = ('transparent')
+        })
+        optionsLampEl.style.background = ('#DEEEEE')
     } else if (optionsLampEl.dataset.action == 2) {
         interierLampEl.src = 'images/lamp2.webp'
         lampEl.src = 'images/lamp2.webp'
+        const siblings = findSiblings(optionsLampEl)
+        siblings.forEach(sibling => {
+            sibling.style.background = ('transparent')
+        })
+        optionsLampEl.style.background = ('#DEEEEE')
     } else if (optionsLampEl.dataset.action == 3) {
-        interierLampEl.src = 'images/lamp3.webp'
-        lampEl.src = 'images/lamp3.webp'
+        interierLampEl.src = 'images/lamp3-2.webp'
+        lampEl.src = 'images/lamp3-2.webp'
+        const siblings = findSiblings(optionsLampEl)
+        siblings.forEach(sibling => {
+            sibling.style.background = ('transparent')
+        })
+        optionsLampEl.style.background = ('#DEEEEE')
+
     }
     }
 })
+
+// Utils functions
+
+function findSiblings(element) {
+    const parent = element.parentElement
+    const children = [...parent.children]
+    const siblings = children.filter(child => child != element)
+    return siblings
+  
+    // return [...element.parentElement.children].filter(child => child != element)
+  }
